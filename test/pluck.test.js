8cb803e283603cf955643459194c610d3be132cd
codepadma <padma.govindarajalu@gmail.com>
2017-05-07 14:08:14 -0700

    add testcases to test pluck when an object of student objects are given

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,14 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  it('returns an array of just grades, given an array of student objects', () => {
+    const students = [
+      {name: 'Leah', grade: 'third'},
+      {name: 'Sara', grade: 'fifth'},
+      {name: 'ChaCha', grade: 'pre-k'}
+    ];
+    const result = ['third', 'fifth', 'pre-k'];
+    expect(result).toEqual(result);
+  });
+
 });
