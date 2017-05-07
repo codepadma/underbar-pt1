e9df488467affa47e7b5696b337bb011b5f29e8d
codepadma <padma.govindarajalu@gmail.com>
2017-05-06 20:24:37 -0700

    add testcases to test when offset is negative and zero

--- test/first.test.js
+++ test/first.test.js
@@ -12,4 +12,12 @@ describe('first()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.first(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns the elements in the array until the last but one', () => {
+    expect(_.first(['a', 'b', 'c'], -1)).toEqual(['a', 'b']);
+  });
+
+  it('returns empty array if you ask for 0 elements', () => {
+    expect(_.first(['a', 'b', 'c'], 0)).toEqual([]);
+  });
 });
