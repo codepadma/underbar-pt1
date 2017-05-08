c5d0c9e1f0993bc89b1a00aa55659f688d113418
codepadma <padma.govindarajalu@gmail.com>
2017-05-07 17:30:01 -0700

    add testcases to test _.some - true/false cases

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,14 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  it('returns true if there is atleast a number which is a multiple of 5', () => {
+  	const nums = [5, 67, 78, 45];
+  	expect(_.some(nums, num => num % 5 === 0)).toBe(true);
+  });
+
+  it('returns false if there is no number which is a multiple of 5', () => {
+  	const nums = [56, 67, 78, 98];
+  	expect(_.some(nums, num => num % 5 === 0)).toBe(false);
+  });
+
 });
